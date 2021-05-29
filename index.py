ef853d5f634f7ec49f939789215ca7470e5ceae1
IkramKhanNiazi <ikramfyp@gmail.com>
2021-05-29 21:37:44 +0500

    Lec 2 Backend

--- index.py
+++ index.py
@@ -1,26 +1,45 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
+from flask import redirect, url_for
+import json
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.route('/')
+API_DATA = '{"data":[{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth","avatar":"https://reqres.in/img/faces/1-image.jpg"},{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver","avatar":"https://reqres.in/img/faces/2-image.jpg"},{"id":3,"email":"emma.wong@reqres.in","first_name":"Emma","last_name":"Wong","avatar":"https://reqres.in/img/faces/3-image.jpg"},{"id":4,"email":"eve.holt@reqres.in","first_name":"Eve","last_name":"Holt","avatar":"https://reqres.in/img/faces/4-image.jpg"},{"id":5,"email":"charles.morris@reqres.in","first_name":"Charles","last_name":"Morris","avatar":"https://reqres.in/img/faces/5-image.jpg"},{"id":6,"email":"tracey.ramos@reqres.in","first_name":"Tracey","last_name":"Ramos","avatar":"https://reqres.in/img/faces/6-image.jpg"}]}'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        session['username'] = request.form['email']
+        return redirect(url_for('home'))
+
+    return render_template('login.html')
 
 
 @app.route('/home')
 def home():
-    return '<a href="/"><h1>Go to Index</h1></a>'
+    if session.get('username'):
+        return render_template('home.html')
+    
+    return render_template('login.html')    
 
 
-@app.route('/user/<user_id>')
-def user(user_id):
-    breakpoint()
-    return f'User Id is{user_id}'
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('index'))
 
 
+@app.route('/data')
+def data():
+    return render_template('data.html', data=json.loads(API_DATA))
 
 if __name__ == '__main__':
    app.run(debug = True)
