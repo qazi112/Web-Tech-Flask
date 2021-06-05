11f648bdc9f1bb04a48de2ef68a36224d84cb615
IkramKhanNiazi <ikramfyp@gmail.com>
2021-06-05 09:01:08 +0500

    Backend - Database

--- index.py
+++ index.py
@@ -3,43 +3,57 @@ from flask import render_template
 from flask import request
 from flask import session
 from flask import redirect, url_for
-import json
+import sqlite3 as sql
+
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-API_DATA = '{"data":[{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth","avatar":"https://reqres.in/img/faces/1-image.jpg"},{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver","avatar":"https://reqres.in/img/faces/2-image.jpg"},{"id":3,"email":"emma.wong@reqres.in","first_name":"Emma","last_name":"Wong","avatar":"https://reqres.in/img/faces/3-image.jpg"},{"id":4,"email":"eve.holt@reqres.in","first_name":"Eve","last_name":"Holt","avatar":"https://reqres.in/img/faces/4-image.jpg"},{"id":5,"email":"charles.morris@reqres.in","first_name":"Charles","last_name":"Morris","avatar":"https://reqres.in/img/faces/5-image.jpg"},{"id":6,"email":"tracey.ramos@reqres.in","first_name":"Tracey","last_name":"Ramos","avatar":"https://reqres.in/img/faces/6-image.jpg"}]}'
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-
-        session['username'] = request.form['email']
-        return redirect(url_for('home'))
-
-    return render_template('login.html')
+@app.route('/add')
+def new_student():
+   return render_template('student.html')
 
 
-@app.route('/home')
+@app.route('/newrecord',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         with sql.connect("database.db") as con:
+            
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html",rows = rows)
+
+
+@app.route('/')
 def home():
-    if session.get('username'):
-        return render_template('home.html')
-    
-    return render_template('login.html')    
+   return render_template('home.html')
 
-
-@app.route('/logout')
-def logout():
-    session.pop('username')
-    return redirect(url_for('index'))
-
-
-@app.route('/data')
-def data():
-    return render_template('data.html', data=json.loads(API_DATA))
 
 if __name__ == '__main__':
    app.run(debug = True)
