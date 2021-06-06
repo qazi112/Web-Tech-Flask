34c47e952d028f5ff6e20b9c03651f411258872c
qazi112 <qarsalan13@gmail.com>
2021-06-06 23:14:06 +0500

    Delete Functionality added

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask import render_template
 from flask import request
 from flask import session
 from flask import redirect, url_for
+
 import sqlite3 as sql
 
+def create_table():
+   conn = sql.connect("database.db")
+   if conn.execute("""CREATE TABLE students(s_id INTEGER PRIMARY KEY AUTOINCREMENT,s_name TEXT NOT NULL,
+      addr TEXT, city TEXT , pincode TEXT );"""):
+      print("Created")
+   else:
+         print("Error")
+   conn.close()
 
 app = Flask(__name__)
-
 
 @app.route('/add')
 def new_student():
@@ -25,7 +33,7 @@ def addrec():
          with sql.connect("database.db") as con:
             
             cur = con.cursor()
-            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            cur.execute("INSERT INTO students (s_name,addr,city,pincode) VALUES (?,?,?,?)",(nm,addr,city,pin) )
             
             con.commit()
             msg = "Record successfully added"
@@ -56,18 +64,36 @@ def list():
 def home():
    return render_template("home.html")
 
+# to delete record
+@app.route("/delete/<s_id>")
+def delete(s_id):
+   student_id = s_id
+   msg = ""
+   try:
+      with sql.connect("database.db") as conn:
+         cur = conn.cursor()
+         query= "DELETE FROM students WHERE s_id = ?;"
+
+         cur.execute(query,(student_id,))
+         conn.commit()
+
+         query = """UPDATE `sqlite_sequence`
+         SET `seq` = (SELECT MAX(`s_id`) FROM 'students')
+         WHERE `name` = 'students';"""
+         cur.execute(query)
+         conn.commit()
+         cur.close()
+         msg = "SUCCESS"
+   except:
+      conn.rollback()
+      print("Error")
+      msg = "error in Deletion operation"
+
+   
+      
+   return redirect(url_for("list"))
+   conn.close()
+
 
 if __name__ == '__main__':
    app.run(debug = True,host="127.0.0.1",port=5051)
-
-
-#### create table 
-
-# import sqlite3
-
-# conn = sqlite3.connect('database.db')
-# print "Opened database successfully";
-
-# conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-# print "Table created successfully";
-# conn.close()
